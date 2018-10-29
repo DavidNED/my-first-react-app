0a1813c37f9488fd4abff73d874dd5c4f2296e3b
David <d_middelveld@hotmail.com>
2018-10-29 16:39:49 +0100

    this

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-        <h1>Welcome to Codaisseur</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+
+    class App extends Component {
+      render() {
+        return (
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title">Welcome to React</h1>
+            </header>
+
+            <main>
+              <Title />
+            </main>
+          </div>
+        );
+      }
+    }
+    // ...
 
 export default App;
